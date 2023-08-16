59476c684c8b871cd68d55d790753c099fea38a9
Michael Malburg <e61806515@student.tuwien.ac.at>
2023-08-16 17:16:47 +0200

    PZ=2 funkt., maxh gross Einf auf ratio round/flat

--- Thesis_deg_sweep.py
+++ Thesis_deg_sweep.py
@@ -6,10 +6,50 @@ from ngsolve import *
 from netgen.meshing import MeshingParameters, IdentificationType
 import numpy as np
 import math as math
-import netgen.gui
 import matplotlib.pyplot as plot
-import cmath
+import netgen.gui
 
+
+############################################
+def drawBndAll(mesh, block=True, old=None, drawFunc=None, useBBnd=False):
+
+    if old == None:
+        old = True if mesh.dim == 2 else False
+    [drawBnd(mesh, x, block, old, drawFunc=drawFunc, useBBnd=useBBnd) for x in set(mesh.GetBoundaries() if not useBBnd else mesh.GetBBoundaries())]
+
+def drawBnd(mesh, name="bottom|right|top|left|ibot|itop|interface|ileft|iright|natural", block=False, old=False, drawFunc=None, useBBnd=False):
+    from ngsolve import CF, H1, GridFunction, BND, BBND, Integrate
+
+    if drawFunc == None:
+        from ngsolve import Draw
+        drawFunc = Draw
+
+
+    val = {bnd:1 for bnd in name.split("|")}
+
+
+    if old:
+        if useBBnd:
+            fes = H1(mesh, dirichlet_bbnd=name)
+            sol = GridFunction(fes, "bbnd")
+            sol.Set(CF([val[bbnd] if bbnd in val.keys() else 0 for bbnd in mesh.GetBBoundaries()]), VOL_or_BND=BBND)
+        else:
+            fes = H1(mesh, dirichlet=name)
+            sol = GridFunction(fes, "bnd")
+            sol.Set(CF([val[bnd] if bnd in val.keys() else 0 for bnd in mesh.GetBoundaries()]), VOL_or_BND=BND)
+        drawFunc(sol)
+        print("-----", name, sum(sol.vec))
+
+    else:
+        bnd_CF = CF([val[bnd] if bnd in val.keys() else 0 for bnd in (mesh.GetBoundaries() if not useBBnd else mesh.GetBBoundaries())])
+        drawFunc(bnd_CF, mesh, "bnd", draw_vol=False)
+        print("-----", name, Integrate(bnd_CF, mesh, VOL_or_BND=(BND if not useBBnd else BBND)))
+
+    if block:
+        #cmdInput(locals(), globals())
+        input()
+
+#########################################
 #ngsglobals.msg_level = 5
 
 
@@ -18,27 +58,39 @@ import cmath
 #
 #
 #
+PZ = 2
+
 def MakeGeometry(H_L, H_M, H_Fe, H_W, Tau, PZ):
     
     print("Tau = ", Tau)
 
     r_Fe = H_Fe + H_W
     r_L = r_Fe + H_L
-    rect = WorkPlane(Axes((-r_L,-r_L,0), n=Z, h=X)).Rectangle(2*r_L, r_L).Face()
+    d_phi = 360/PZ
+    phi_M = d_phi*Tau/2
+    #rect = WorkPlane(Axes((-r_L,-r_L,0), n=Z, h=X)).Rectangle(2*r_L, r_L).Face()
+    subtract = WorkPlane(Axes((0,0,0), n=Z, h=Y)).Rotate(d_phi/2)
+    #subtract.MoveTo((r_Fe+H_M)*sin(d_phi/2*pi/180), (r_Fe+H_M)*cos(d_phi/2*pi/180))
+    subtract.Line(r_L).Rotate(90)
+    subtract.Arc(r_L, (360-d_phi)).Rotate(90)
+    subtract.Line(r_L)
+    #subtract.Line(r_L).Rotate(180+d_phi)
+    #subtract.Line(r_L)
+    subtract = subtract.Face()
 
-    inner = WorkPlane().Circle(H_W).Face() - rect
+    inner = WorkPlane().Circle(H_W).Face() - subtract
     inner.edges.name="inner"
 
     rotor = WorkPlane().Circle(r_Fe).Face() #-rect
     rotor.name = "rotor"
-    rotor = rotor - rect
+    rotor = rotor - subtract
     rotor.col = (1,0,0)
 
     magnets =  []#[WorkPlane(Axes((0,0,0), n=Z, h=X))] * PZ
-    d_phi = 360/PZ
-    phi_M = d_phi*Tau/2
+    right = 360/(PZ*2)
+    left = 360*(1-1/(PZ*2))
     for i in range(PZ):
-        if (i*d_phi < 90) or (i*d_phi > 270):
+        if (i*d_phi < right) or (i*d_phi > left):
             magnets = magnets + [WorkPlane(Axes((0,0,0), n=Z, h=X))]
             magnets[i].MoveTo(r_Fe*sin(i*d_phi*pi/180), r_Fe*cos(i*d_phi*pi/180))
             magnets[i].Arc(r_Fe, -phi_M).Rotate(90)
@@ -48,6 +100,7 @@ def MakeGeometry(H_L, H_M, H_Fe, H_W, Tau, PZ):
             magnets[i].Arc(r_Fe, -phi_M).Rotate(-d_phi)
             magnets[i] = magnets[i].Face() #- rect
             magnets[i].maxh = 0.001
+            magnets[i].edges.name = "magnet_edge"
             #magnets[i].vertices.maxh = 0.00001 #geht nicht??
             magnets[i].name = f"magnets_{i}" #{i:"Anweisungen zur Formatierung"} Formated String
             magnets[i].col = (0, 0, 1)
@@ -55,7 +108,7 @@ def MakeGeometry(H_L, H_M, H_Fe, H_W, Tau, PZ):
        
     outer = WorkPlane().Circle(r_L).Face() 
     outer.name = "air"
-    outer = outer - rect
+    outer = outer - subtract
     outer.edges.name = "outer"
     outer.edges
 
@@ -66,70 +119,59 @@ def MakeGeometry(H_L, H_M, H_Fe, H_W, Tau, PZ):
     
 
     geo = Glue(([outer-rotor - sum(magnets), rotor- inner] + magnets))
+    
     #geo = Glue([geo - rect])
     #for i in range(PZ):
     #    geo = Glue([geo, magnets[i]])
     #PERIODIC EDGES
-    trafo = Rotation(Axis((0,0,0), Z), 180)
+    trafo = Rotation(Axis((0,0,0), Z), -d_phi)
 
     if Tau<1 :
-        a = geo.edges.Nearest((-(r_Fe+H_L/2),0,0))
+        a = geo.edges.Nearest((-(r_Fe+H_L/2)*sin(d_phi/2*pi/180),(r_Fe+H_L/2)*cos(d_phi/2*pi/180),0))
         a.name = "left_o"
-        b = geo.edges.Nearest((-(r_Fe/2),0,0)) 
+        b = geo.edges.Nearest((-(r_Fe/2)*sin(d_phi/2*pi/180),(r_Fe/2)*cos(d_phi/2*pi/180),0)) 
         b.name = "left_i"
-        d = geo.edges.Nearest(((r_Fe+H_L/2),0,0))
+        d = geo.edges.Nearest(((r_Fe+H_L/2)*sin(d_phi/2*pi/180),(r_Fe+H_L/2)*cos(d_phi/2*pi/180),0))
         d.name = "right_o"
-        c = geo.edges.Nearest(((r_Fe/2),0,0))
+        c = geo.edges.Nearest(((r_Fe/2)*sin(d_phi/2*pi/180),(r_Fe/2)*cos(d_phi/2*pi/180),0))
         c.name = "right_i"
     else:
-        a = geo.edges.Nearest((-(r_Fe+H_L - (H_L-H_M)/2),0,0))
+        a = geo.edges.Nearest((-(r_Fe+H_L - (H_L-H_M)/2)*sin(d_phi/2*pi/180),(r_Fe+H_L - (H_L-H_M)/2)*cos(d_phi/2*pi/180),0))
         a.name = "left_o"
-        b = geo.edges.Nearest((-(r_Fe/2),0,0)) 
+        b = geo.edges.Nearest((-(r_Fe/2)*sin(d_phi/2*pi/180),(r_Fe/2)*cos(d_phi/2*pi/180),0)) 
         b.name = "left_i"
-        e = geo.edges.Nearest((-(r_Fe+H_M/2),0,0))
+        e = geo.edges.Nearest((-(r_Fe+H_M/2)*sin(d_phi/2*pi/180),(r_Fe+H_M/2)*cos(d_phi/2*pi/180),0))
         e.name = "left_m"
-        f = geo.edges.Nearest(((r_Fe+H_M/2),0,0))
+        f = geo.edges.Nearest(((r_Fe+H_M/2)*sin(d_phi/2*pi/180),(r_Fe+H_M/2)*cos(d_phi/2*pi/180),0))
         f.name = "right_m"
-        d = geo.edges.Nearest(((r_Fe+H_L/2 - (H_L-H_M)/2),0,0))
+        d = geo.edges.Nearest(((r_Fe+H_L - (H_L-H_M)/2)*sin(d_phi/2*pi/180),(r_Fe+H_L - (H_L-H_M)/2)*cos(d_phi/2*pi/180),0))
         d.name = "right_o"
-        c = geo.edges.Nearest(((r_Fe/2),0,0))
+        c = geo.edges.Nearest(((r_Fe/2*sin(d_phi/2*pi/180)),(r_Fe/2*cos(d_phi/2*pi/180)),0))
         c.name = "right_i"
 
         e.Identify(f, "magnet_edge", type = IdentificationType.PERIODIC, trafo=trafo)
     
     a.Identify(d, "air_edge",type = IdentificationType.PERIODIC, trafo=trafo)
-    b.Identify(c, "rotor_edge",type = IdentificationType.PERIODIC, trafo=trafo)
+    b.Identify(c, "rotor_edge",type = IdentificationType.PERIODIC, trafo=trafo) 
     
     geo.edges.maxh=0.0005
     return geo
 
-PZ = 2 
+
 mp = MeshingParameters(maxh=0.004)
 #mp.RestrictH(x=0, y=0, z=1, h=0.0025)
-phase = [-1,-1]
+
 
 #radius Rotor ist bei Schmid 35.19mm, r_rot = H_Fe+H_W
-mesh = Mesh(OCCGeometry(MakeGeometry(H_L=8e-3, H_M=6e-3, H_Fe=26.19e-3, H_W=9e-3, Tau=3/4, PZ=PZ), dim = 2).GenerateMesh(mp=mp))
+geo = MakeGeometry(H_L=8e-3, H_M=6e-3, H_Fe=26.19e-3, H_W=9e-3, Tau=3/4, PZ=PZ)
+mesh = Mesh(OCCGeometry(geo, dim = 2).GenerateMesh(mp=mp, maxh=0.001))
 mesh.Curve(3)
-#Materials
-#('air', 'rotor', 'magnets_0', 'magnets_1', 'magnets_2', 'magnets_3', 'magnets_4', 'magnets_5', 'magnets_6', 'magnets_7', 'magnets_8', 'magnets_9', 'magnets_10', 'magnets_11')
-#Boundaries
-#{'default', 'inner', 'outer'}
+
 
 print(mesh.GetMaterials())
 print(set(mesh.GetBoundaries()))
 
 
-#              Coefficient Functions Sigma und Mu
-#
-#                   magnet:NdFeB mu_r = 1.04 - 1.08 und sigma = 0.667e6
-#                   rotor: Fe mu_r = 1e4 oder 5e3 u. sigma = 1.76e6 - 1.97e6 .... Welche Werte Herr Schmid??????
-#
-#version_0 = CF([0, 13, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
-#version_1 = CF(list(range(len(mesh.GetMaterials()))))
-#mu_air = 4e-7*np.pi
-#mu_magnet = 4e-7*np.pi#1.08*mu_air
-#mu_rotor = 4e-7*np.pi#mu_air*5e3
 mu_air = 4e-7*np.pi
 mu_magnet = 1.08*mu_air
 mu_rotor = mu_air*5e3
@@ -137,7 +179,7 @@ mu = {"air":mu_air, "rotor": mu_rotor}
 [mu.update({f"magnets_{i}": mu_magnet}) for i in range(PZ)]
 muCF = mesh.MaterialCF(mu, default=mu_air)
 
-sigma = {"air":0, "rotor": 1.86e6}
+sigma = {"air":1e-3, "rotor": 1.86e6}
 [sigma.update({f"magnets_{i}": 0.667e6}) for i in range(PZ)]
 sigmaCF = mesh.MaterialCF(sigma, default=0)
 sigma_v = {"air":None, "rotor": None}
@@ -145,7 +187,6 @@ sigma_v = {"air":None, "rotor": None}
 sigma_visual = mesh.MaterialCF(sigma_v)
 
 versions = [muCF, sigmaCF]
-
 Br = 1
 K0= 10000
 
@@ -172,8 +213,11 @@ test = V.TestFunction()
 
 x_val = np.logspace(0, 6, 100)
 p_values=[]
-for omega in x_val: 
-        
+p_flat =np.loadtxt('p_flat.csv', delimiter=',')
+print(len(p_flat))
+
+for omega in x_val:
+
     u = GridFunction(V) 
 
     a = BilinearForm(V, symmetric = True)
@@ -183,21 +227,23 @@ for omega in x_val:
     c = Preconditioner(a, type="direct", inverse = "sparsecholesky")    
 
     f = LinearForm(V)
-    f += K(x,y)*test.Trace()*ds("outer") #*cos(2/D*x) PROBLEM weil hier periodische RBs
-
+    f += K(x,y)*test.Trace()*ds("outer")
     with TaskManager():
             a.Assemble()
             f.Assemble()
             bvp = BVP(bf=a, lf=f, gf=u, pre=c)
             bvp.Do()
-
+    
     p = sigmaCF*omega*omega*u*Conj(u)/2
     energy = Integrate(p, mesh, definedon=mesh.Materials("magnets_0"))*2
-
+    
     p_values.append(energy.real)
 
-p_flat =np.loadtxt('p_flat.csv', delimiter=',')
-plot.plot(x_val, p_values, )
+
+
+
+
+plot.plot(x_val, p_values)
 plot.title('Eddy Current 1st O. Losses Half-Round, Tau=3/4, PZ=2')
 plot.xscale('log')
 plot.yscale('log')
@@ -206,6 +252,7 @@ plot.ylabel('Power Losses (W/m)')
 plot.grid(True, which='both')
 plot.savefig('Losses_Round_180_Deg.jpg', format='jpg')
 plot.show()
+
 
 r_vs_f=[]
 for i in np.arange(0,len(x_val)):
